e4b17c76c683914a55bb938e4602db41f584e2ca
kimothokamau <kimothokamau@protonmail.com>
2020-02-13 05:14:47 +0300

    Frontend build compile

---
+++ frontend/build/precache-manifest.2e40b6990904fd5391d63a1c72016133.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7ed94ff5220e100601d969edf631757",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28ad236ebab4be99f5d2",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "75b150358a64f6271292",
+    "url": "/static/css/main.c206220d.chunk.css"
+  },
+  {
+    "revision": "28ad236ebab4be99f5d2",
+    "url": "/static/js/2.7a6f0a65.chunk.js"
+  },
+  {
+    "revision": "75b150358a64f6271292",
+    "url": "/static/js/main.582387f9.chunk.js"
+  },
+  {
+    "revision": "01137154c2948edbb70f",
+    "url": "/static/js/runtime-main.42a8a048.js"
+  }
+]);
